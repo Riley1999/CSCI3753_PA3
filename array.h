3ba5ef9e89d4c1ab01ca8bd8f16ea55fc7d7c703
Riley1999 <riko3469@colorado.edu>
2022-03-12 23:28:21 -0700

    x

--- array.h
+++ array.h
@@ -1,19 +1,19 @@
 #ifndef STACK_H
 #define STACK_H
 
-#define STACK_SIZE 8                       // max elements in stack
+#define STACK_SIZE 8                               // max elements in stack
 #define MAX_NAME_LENGTH 30 
 
 typedef struct {
-    char **array[STACK_SIZE];                  // storage array for integers
-    int top;                                // array index indicating where the top is
-} stack;
+    char **array[STACK_SIZE];                      // storage array for integers
+    int top;                                       // array index indicating where the top is
 
-int  stack_init(stack *s);                  // init the stack
-int  stack_push(stack *s, char *hostname);     // place element on the top of the stack
-int  stack_pop (stack *s, char **hostname);    // remove element from the top of the stack
-int  is_full(stack *s);
-void display(stack *s);
-void stack_free(stack *s);                  // free the stack's resources
+    int  stack_init(stack *s);                     // init the stack
+    int  stack_push(stack *s, char *hostname);     // place element on the top of the stack
+    int  stack_pop (stack *s, char **hostname);    // remove element from the top of the stack
+    int  is_full(stack *s);
+    void display(stack *s);
+    void stack_free(stack *s);                     // free the stack's resources
+} stack;
 
 #endif
